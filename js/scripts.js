85a9f038a2759d9ab7276f90a38125c6a8c0ec63
PCRVLX <imhobff@gmail.com>
2022-08-26 17:01:38 +0300

    create and connect JsQuery connect carousel for section sellers finish adaptive

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            834:{
+                items:2
+            },
+            820:{
+                items:2
+            },
+            800:{
+                items:2
+            },
+            768:{
+                items:2
+            },
+
+            1000:{
+                items:3
+            }
+        }
+    });
+});
